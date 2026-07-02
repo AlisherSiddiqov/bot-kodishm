777a44a9696f7b94102fdb69b52c3540046aa1bc
AlisherSiddiqov <161743528+AlisherSiddiqov@users.noreply.github.com>
2026-07-02 12:46:09 +0500

    Refactor states in states.py for consistency

--- states.py
+++ states.py
@@ -1,19 +1,19 @@
 from aiogram.fsm.state import State, StatesGroup
- 
+
 class AddMovie(StatesGroup):
     code = State()
     caption = State()
     video = State()
     confirm = State()
- 
+
 class Premium(StatesGroup):
     choosing_tariff = State()
     sending_check = State()
- 
+
 class Broadcast(StatesGroup):
     waiting_content = State()
+    waiting_code = State()
     confirm = State()
- 
+
 class SetLang(StatesGroup):
     choosing = State()
- 
